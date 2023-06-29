63405925a24087ecc9e5f50b5126ff9742870eb3
siluye <seife@yekicomp.com>
2023-06-29 19:23:32 +0000

    update

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const array2 = [];
-  for (const value of array) {
-    array2.push(appendString + value);
-  }
-  return array2;
-}
